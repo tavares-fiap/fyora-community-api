79789ce114f8a1dee60a8ef00278f13cbcc9f611
tavares-fiap <rm97877@fiap.com>
2025-09-20 16:29:21 -0300

    Primeiro commit: Projeto base springboot, docker compose.yml para subir um db postgresql

---
+++ src/main/java/com/fyora/community/CommunityApiApplication.java
@@ -0,0 +1,13 @@
+package com.fyora.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunityApiApplication.class, args);
+	}
+
+}
